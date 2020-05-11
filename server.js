312961d588baadfc986f458188e8e326f5f1bfc9
Vitor Maluf <vitorzamignanim@gmail.com>
2020-05-11 16:07:39 -0300

    remocao de comentarios

--- server.js
+++ server.js
@@ -1,59 +1,9 @@
 /*
-const connection = mysql.createConnection({
-  host: 'engsoft2020.mysql.dbaas.com.br',
-  user: 'engsoft2020',
-  password: 'a123456',
-  database: 'engsoft2020'
-});
 tipos:
 0 - amdin
 1 - aluno
 2 - tutor
 3 - pai
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Conectado');
-});
-
-connection.end();
-------------------------------------------------------------
-CRUD:
-READ: 
-  connection.query('SELECT * FROM authors', (err,rows) => {
-  if(err) throw err;
-
-  console.log('Data received from Db:');
-  console.log(rows);
-});
-
-CREATE:
-const author = { name: 'Craig Buckler', city: 'Exmouth' };
-connection.query('INSERT INTO authors SET ?', author, (err, res) => {
-  if(err) throw err;
-
-  console.log('Last insert ID:', res.insertId);
-});
-
-UPDATE: 
-connection.query(
-  'UPDATE authors SET city = ? Where ID = ?',
-  ['Leipzig', 3],
-  (err, result) => {
-    if (err) throw err;
-
-    console.log(`Changed ${result.changedRows} row(s)`);
-  }
-);
-
-DELETE: 
-connection.query(
-  'DELETE FROM authors WHERE id = ?', [5], (err, result) => {
-    if (err) throw err;
-
-    console.log(`Deleted ${result.affectedRows} row(s)`);
-  }
-);
 */
 
 const http = require('http');
